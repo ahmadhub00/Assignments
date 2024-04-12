1afa7011004c851d8bdef400c9fe73f600cf0925
Ahmad Taimur <ullahtaimur@gmail.com>
2024-04-12 12:00:00 +0000

    commit

--- Dijkstra's-algo.js
+++ Dijkstra's-algo.js
@@ -42,3 +42,13 @@ function dijkstra(graph, start) {
     return distances;
 }
 
+let weightedGraph = {
+    A: { B: 1, C: 4 },
+    B: { A: 1, D: 2, E: 5 },
+    C: { A: 4, F: 3 },
+    D: { B: 2 },
+    E: { B: 5, F: 1 },
+    F: { C: 3, E: 1 }
+};
+
+console.log(dijkstra(weightedGraph, "A"));
